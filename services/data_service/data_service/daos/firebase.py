c4e2aa410f8339fb36eb2bc3205daf6c6a96dcd8
Luca Galbusera <lucagalbu@gmail.com>
2021-12-19 15:35:27 +0100

    [data_service]DAO for Google Firebase

--- services/data_service/data_service/daos/firebase.py
+++ services/data_service/data_service/daos/firebase.py
@@ -1,6 +1,11 @@
 """Implements the DAO using Google Firebase"""
 from __future__ import annotations
+from typing import TYPE_CHECKING
 from firebase_admin import storage, App, initialize_app
+from data_service.daos.base import DaoBase
+
+if TYPE_CHECKING:
+    import google.cloud.storage
 
 
 class FirebaseClient:
@@ -20,3 +25,25 @@ class FirebaseClient:
     def bucket(self):
         """Return a Firebase Bucket from default app"""
         return storage.bucket()
+
+
+class FirebaseDao(DaoBase):
+    """Firebase DAO"""
+
+    bucket: google.cloud.storage.Bucket
+
+    def __init__(self, firebase_client: FirebaseClient):
+        self.bucket = firebase_client.bucket
+
+    def upload_text(self, filename: str, content: str):
+        blob = self.bucket.blob(blob_name=filename)
+        blob.upload_from_string(data=content)
+
+    def download_text(self, filename: str) -> str:
+        blob = self.bucket.blob(blob_name=filename)
+        text = blob.download_as_text()
+        return text
+
+    def delete_file(self, filename: str) -> None:
+        blob = self.bucket.blob(blob_name=filename)
+        blob.delete()
