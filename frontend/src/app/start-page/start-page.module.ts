17ef1be271916927b6a1136b58fe91640d237c07
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 12:30:29 +0100

    [front_end] BookCard: Added action buttons

--- frontend/src/app/start-page/start-page.module.ts
+++ frontend/src/app/start-page/start-page.module.ts
@@ -4,12 +4,19 @@ import { RouterModule } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { StartPageComponent } from './start-page.component';
 import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   declarations: [StartPageComponent, BookCardComponent],
-  imports: [CommonModule, RouterModule, MatCardModule, MatProgressBarModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatButtonModule,
+  ],
 })
 export class StartPageModule {}
