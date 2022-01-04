3495b6d7f20da8f5d1fcc3217d3073788c74bb5a
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 14:46:15 +0100

    [front_end] Added BookSummary component
    The BookSummary component is used by the StartPage to display detailed
    information about a specific book.

---
+++ frontend/src/app/start-page/book-summary/book-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-summary',
+  templateUrl: './book-summary.component.html',
+  styleUrls: ['./book-summary.component.scss'],
+})
+export class BookSummaryComponent implements OnInit {
+  ngOnInit(): void {}
+}
