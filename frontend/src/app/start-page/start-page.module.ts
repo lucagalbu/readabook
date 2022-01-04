b9b45b9872b9b2fd14b4430a01ae9538d6088128
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 12:28:36 +0100

    [front_end] BookCard: added content section

--- frontend/src/app/start-page/start-page.module.ts
+++ frontend/src/app/start-page/start-page.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 import { StartPageComponent } from './start-page.component';
 import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   declarations: [StartPageComponent, BookCardComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatCardModule, MatProgressBarModule],
 })
 export class StartPageModule {}
