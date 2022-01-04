bf721e0e7a471ecf648519a1d9862e88214e8d0f
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 18:14:29 +0100

    [front_end] Added Reader module
    This module deals with showing a book content and a dictionary of the
    words.

---
+++ frontend/src/app/reader/reader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reader',
+  templateUrl: './reader.component.html',
+  styleUrls: ['./reader.component.scss'],
+})
+export class ReaderComponent {}
