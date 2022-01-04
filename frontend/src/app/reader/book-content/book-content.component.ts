f5be90778c2940381d959ae47c945fbd8ac3bdb5
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 18:42:30 +0100

    [front_end] BookContent: Added component
    Initiliazation of an empty component that will display the content of
    a book.

---
+++ frontend/src/app/reader/book-content/book-content.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'book-content',
+  templateUrl: './book-content.component.html',
+  styleUrls: ['./book-content.component.scss'],
+})
+export class BookContentComponent implements OnInit {
+  ngOnInit(): void {}
+}
