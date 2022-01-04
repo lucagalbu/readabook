3091cbbf690e099371c74ec358cc90bae8be65a5
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 19:17:10 +0100

    [front_end] Dictionary: empty card
    Added an empty card that will store all the UI elements of the
    Dictionary component.

--- frontend/src/app/reader/reader.module.ts
+++ frontend/src/app/reader/reader.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+
 import { ReaderComponent } from './reader.component';
 import { BookContentComponent } from './book-content/book-content.component';
 import { DictionaryComponent } from './dictionary/dictionary.component';
 
 @NgModule({
   declarations: [ReaderComponent, BookContentComponent, DictionaryComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
 })
 export class ReaderModule {}
