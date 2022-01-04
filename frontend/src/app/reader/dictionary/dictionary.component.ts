990e1f959c369482a7311fde2716e6a4113af40d
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 19:09:48 +0100

    [front_end] Dictionary: component initialization

---
+++ frontend/src/app/reader/dictionary/dictionary.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dictionary',
+  templateUrl: './dictionary.component.html',
+  styleUrls: ['./dictionary.component.scss'],
+})
+export class DictionaryComponent implements OnInit {
+  ngOnInit(): void {}
+}
