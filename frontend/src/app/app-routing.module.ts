dadbcf4d5677b7a8d453cd3eabbaa92b0c675ef7
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 18:31:43 +0100

    [front_end] Reader Module: added to router
    The view implemented in the Reader Module has been added to the router.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { StartPageComponent } from './start-page/start-page.component';
+import { ReaderComponent } from './reader/reader.component';
 
 const appRoutes: Routes = [
   { path: 'start-page', component: StartPageComponent },
+  { path: 'reader/:filename', component: ReaderComponent },
   { path: '', redirectTo: '/start-page', pathMatch: 'full' },
 ];
 
