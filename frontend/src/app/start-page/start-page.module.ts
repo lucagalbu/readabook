6097ab298ba952e3a72949ac8f56ce86ed8e086f
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 15:00:13 +0100

    [front_end] BookSummary: text content
    I added the text content of the component. This displays some stats
    of the book.

--- frontend/src/app/start-page/start-page.module.ts
+++ frontend/src/app/start-page/start-page.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { StartPageComponent } from './start-page.component';
 import { BookCardComponent } from './book-card/book-card.component';
@@ -18,6 +19,7 @@ import { BookSummaryComponent } from './book-summary/book-summary.component';
     MatCardModule,
     MatProgressBarModule,
     MatButtonModule,
+    MatDividerModule,
   ],
 })
 export class StartPageModule {}
