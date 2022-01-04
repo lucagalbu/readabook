87790ee4dc4b09670c16eed4a8a133f657439232
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 12:02:19 +0100

    [front_end] StartPage methods to handle BookCard
    I added to the StartPage the methods and variables required by the
    input and output props of the Bookcard component.

--- frontend/src/app/start-page/start-page.component.ts
+++ frontend/src/app/start-page/start-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StartPageService } from './start-page.service';
+import { Book, WordStats } from './interfaces';
 
 @Component({
   selector: 'start-page',
@@ -7,5 +8,23 @@ import { StartPageService } from './start-page.service';
   styleUrls: ['./start-page.component.scss'],
 })
 export class StartPageComponent {
+  books: Book[] = [];
+  currentBook: Book | undefined;
+  words: WordStats = [];
+
   constructor(private service: StartPageService) {}
+
+  ngOnInit(): void {
+    this.books = this.service.getBooks();
+    this.selectBook(this.books[0]);
+  }
+
+  bookCardClicked(book: Book): void {
+    if (book !== this.currentBook) this.selectBook(book);
+  }
+
+  private selectBook(book: Book): void {
+    this.currentBook = book;
+    this.words = this.service.getWordStats(book.filename, 10);
+  }
 }
