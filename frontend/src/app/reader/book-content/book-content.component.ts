44a0cf70933eebacdf54b7e586a6844193b68491
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 18:53:06 +0100

    [front_end] BookContent: event on word selection
    When a word is selected, an event is raised with the selected word.

--- frontend/src/app/reader/book-content/book-content.component.ts
+++ frontend/src/app/reader/book-content/book-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Book } from '../interfaces';
 
 @Component({
@@ -8,10 +8,36 @@ import { Book } from '../interfaces';
 })
 export class BookContentComponent implements OnInit {
   @Input() book: Book;
+  @Output() wordSelected = new EventEmitter<string>();
 
   constructor() {
     this.book = { title: '', author: '', content: '' };
   }
 
   ngOnInit(): void {}
+
+  onClick(e: MouseEvent) {
+    const selection = window.getSelection()?.toString();
+    if (!selection) return;
+
+    const word = BookContentComponent.getWordFromSelection(selection);
+    if (!word) return;
+
+    this.wordSelected.emit(word);
+  }
+
+  private static getWordFromSelection(selection: string): string | null {
+    let words: string[] | null;
+    if (selection !== '') {
+      words = selection.trim().split(' ');
+    } else {
+      words = null;
+    }
+
+    if (words && words.length === 1) {
+      return words[0];
+    } else {
+      return null;
+    }
+  }
 }
