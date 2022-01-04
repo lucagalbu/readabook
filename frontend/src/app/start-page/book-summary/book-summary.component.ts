0e69fc032ce63d29ac4fab49300f47329bcef68f
Luca Galbusera <lucagalbu@gmail.com>
2022-01-04 18:08:08 +0100

    [front_end] BookSummary: logic for frequency chart
    I added the necessary variables and methods to generate the chart with
    the most frequent words.

--- frontend/src/app/start-page/book-summary/book-summary.component.ts
+++ frontend/src/app/start-page/book-summary/book-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Book, WordStats } from '../interfaces';
+import { ChartOptions, ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-book-summary',
@@ -10,5 +11,96 @@ export class BookSummaryComponent implements OnInit {
   @Input() book: Book | undefined = undefined;
   @Input() words: WordStats = [];
 
+  data: ChartData = { datasets: [] };
+  options: ChartOptions = {};
+
+  constructor() {
+    this.makeChartData();
+    this.makeChartOptions();
+  }
+
   ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    this.makeChartData();
+  }
+
+  private makeChartData() {
+    this.data = {
+      datasets: [
+        {
+          type: 'bar',
+          //@ts-expect-error
+          data: this.words,
+        },
+      ],
+    };
+  }
+
+  private makeChartOptions() {
+    this.options = {};
+
+    this.options = {
+      indexAxis: 'y',
+    };
+
+    this.options.plugins = this.pluginsOptions();
+    this.options.scales = {
+      yAxis: this.yAxisOptions(),
+      xAxis: this.xAxisOptions(),
+    };
+    this.options.parsing = this.parsingOptions();
+    this.options.elements = {
+      bar: this.barOptions(),
+    };
+  }
+
+  private pluginsOptions() {
+    const plugins = {
+      tooltip: {
+        enabled: false,
+      },
+      legend: {
+        display: false,
+      },
+    };
+    return plugins;
+  }
+
+  private yAxisOptions() {
+    const yAxis = {
+      reverse: false,
+    };
+    return yAxis;
+  }
+
+  private xAxisOptions() {
+    const xAxis = {
+      ticks: {
+        callback: BookSummaryComponent.yLabelFormatter,
+      },
+    };
+    return xAxis;
+  }
+
+  private parsingOptions() {
+    const parsing = {
+      xAxisKey: 'freq',
+      yAxisKey: 'word',
+    };
+    return parsing;
+  }
+
+  private barOptions() {
+    const bar = {
+      borderWidth: 2,
+    };
+    return bar;
+  }
+
+  private static yLabelFormatter(value: number | string): string {
+    const valueNum = value as number;
+    const percentage = (valueNum * 100).toFixed(0) + '%';
+    return percentage;
+  }
 }
