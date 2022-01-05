236321152f48651d9153f44cc011be4bf274a93f
Luca Galbusera <lucagalbu@gmail.com>
2022-01-05 12:30:35 +0100

    [front_end] App: Initialized service class
    I intiliazed an abstract class for the services.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { AppService } from './app.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private service: AppService) {}
+
   onViewActivate() {
     document.querySelector('#main-scrollable-container')!.scrollTo(0, 0);
   }
