3e5c603e3c514aa28dfd2e3cbb2bd5592512c35f
Luca Galbusera <lucagalbu@gmail.com>
2022-01-05 12:36:49 +0100

    [front_end] App: Mocked services

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
+import { forwardRef, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+  useClass: forwardRef(() => AppServiceMocked),
+})
+export abstract class AppService {
+  abstract uploadBook(file: File): void;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export abstract class AppService {
-  abstract uploadBook(file: File): void;
+export class AppServiceMocked extends AppService {
+  uploadBook(file: File): void {
+    alert(`
+    File uploaded
+    File name: ${file.name}
+    File type: ${file.type}
+    `);
+  }
 }
