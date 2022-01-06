52d4bff8a8218338a73d2a252327dd0bf5b51054
Luca Galbusera <lucagalbu@gmail.com>
2022-01-06 12:56:37 +0100

    [front_end] NewBook: Initialized component
    I Initialized the component to show the form to upload a new book.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewBookComponent } from './new-book/new-book.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,7 +15,7 @@ import { ReaderModule } from './reader/reader.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NewBookComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
