5d0783244d9df0fb78ca71888637d8880bb8b1e5
Luca Galbusera <lucagalbu@gmail.com>
2022-01-09 22:04:45 +0100

    [data_service] DAO: upload book from string
    I created the functions to upload a book reading the content from a
    string. In this way I implement the abstract method upload_from_string
    of the base DAO class.

--- services/data_service/data_service/daos/firebase.py
+++ services/data_service/data_service/daos/firebase.py
@@ -8,10 +8,12 @@ is raised.
 - The app config is read from the json specified in the .env file.
 - The DAO retrieves the bucket, whose name is read from the app config.
 """
+import hashlib
 import firebase_admin
-from firebase_admin import App, storage
+from firebase_admin import App, storage, db
 from google.cloud.storage.bucket import Bucket
 from data_service.daos.base import DaoBase
+from data_service.typings.book_info import BookInfo
 
 
 class FirebaseDao(DaoBase):
@@ -33,6 +35,23 @@ class FirebaseDao(DaoBase):
                 Check if this DAO has already been initialized and if the authorization is correct."
 
             raise RuntimeError(error) from exc
+
+    def upload_from_string(self, content: str, book_info: BookInfo) -> None:
+        """Upload a book to Firebase. It creates the file in the cloud and
+        add the info to the database. The filename name is the md5 of the content."""
+        md5_content = hashlib.md5(content.encode()).hexdigest()
+        self._add_string_to_bucket(filename=md5_content, content=content)
+        self._add_book_to_db(book_info=book_info, name=md5_content)
+
+    def _add_string_to_bucket(self, filename: str, content: str):
+        """Add a file to the cloud storage reading the content from a string."""
+        blob = self.bucket.blob(blob_name=filename)
+        blob.upload_from_string(data=content)
+
+    def _add_book_to_db(self, book_info: BookInfo, name: str):
+        """Add an entry to the db with the book info"""
+        db_ref = db.reference("/books")
+        db_ref.set({name: {"title": book_info.title, "author": book_info.author}})
 
     def _get_bucket(self):
         """Get the bucket associated to the firebase account and specified in the global setting.
