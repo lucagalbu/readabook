8263b5f96ac55d1a34c3b18156157460430b4ba3
Luca Galbusera <lucagalbu@gmail.com>
2022-01-09 22:06:50 +0100

    [data_service] Services: upload book from string

--- services/data_service/data_service/services/storage.py
+++ services/data_service/data_service/services/storage.py
@@ -1,5 +1,7 @@
 """Business logic of the data service"""
+from typing import Union
 from data_service.daos.base import DaoBase
+from data_service.typings.book_info import BookInfo
 
 
 class StorageService:
@@ -9,3 +11,17 @@ class StorageService:
 
     def __init__(self, dao: DaoBase):
         self.dao = dao
+
+    def upload(
+        self, info: BookInfo, content_type: str, content: Union[str, bytes]
+    ) -> None:
+        """upload a text to a file in the database"""
+        if content_type == "text/plain":
+            content_str = content if isinstance(content, str) else content.decode()
+            self._upload_from_string(info=info, content=content_str)
+        else:
+            raise NotImplementedError(f"Upload of {content_type} is not supported")
+
+    def _upload_from_string(self, info: BookInfo, content: str) -> None:
+        """Upload a book whose content is read from a string"""
+        self.dao.upload_from_string(content=content, book_info=info)
