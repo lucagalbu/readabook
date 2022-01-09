42b55863f7ccae4a77a2f04d93b81d8a22a8c98f
Luca Galbusera <lucagalbu@gmail.com>
2022-01-09 21:11:52 +0100

    [data_service] Dao base: upload_from_string
    Method to upload a book from a string.

--- services/data_service/data_service/daos/base.py
+++ services/data_service/data_service/daos/base.py
@@ -1,6 +1,17 @@
 """Defines the interface all the DAOs must comply with"""
-from abc import ABC
+from abc import ABC, abstractmethod
+from data_service.typings.book_info import BookInfo
 
 
 class DaoBase(ABC):
     """Dao interface"""
+
+    @abstractmethod
+    def upload_from_string(self, content: str, book_info: BookInfo) -> None:
+        """
+        Upload a file to the database, reading the content as a string.
+        It creates the entry with the book info in the realtime database,
+        and a file with the book content in the cloud storage. The filename
+        is the md5 of the content.
+        """
+        ...
