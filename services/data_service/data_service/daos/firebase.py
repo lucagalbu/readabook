48c187db4c4916ff06b29deba6715fe9a78a2062
Luca Galbusera <lucagalbu@gmail.com>
2022-01-09 16:23:56 +0100

    [data_service] Firebase DAO: initialization
    Added a __init__ method to the Firebase DAO.

--- services/data_service/data_service/daos/firebase.py
+++ services/data_service/data_service/daos/firebase.py
@@ -1,6 +1,30 @@
-"""Implements the DAO using Google Firebase"""
+"""
+Implements the DAO using Google Firebase.
+
+- The DAO is supposed to be created only once. If it is created multiple
+times, the firebase app is initialized multiple times and an exception
+is raised.
+- The authorization is read from the .env file.
+"""
+import firebase_admin
+from firebase_admin import App
 from data_service.daos.base import DaoBase
 
 
 class FirebaseDao(DaoBase):
     """Firebase DAO"""
+
+    app: App
+
+    def __init__(self):
+        self._init_app()
+
+    def _init_app(self):
+        """Init a Firebase app and rises an exception if the app has already been initialized."""
+        try:
+            self.app = firebase_admin.initialize_app()
+        except ValueError as exc:
+            error = "Firebase app already in use or error in creating it. \
+                Check if this DAO has already been initialized and if the authorization is correct."
+
+            raise RuntimeError(error) from exc
