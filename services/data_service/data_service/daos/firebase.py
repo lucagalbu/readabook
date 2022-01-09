4c3721e867d8a56668b2877c6328ff22f4542814
Luca Galbusera <lucagalbu@gmail.com>
2022-01-09 16:38:48 +0100

    [data_service] FirebaseDao: Get the bucket
    The DAO gets a reference to the bucket of the cloud storage in Firebase
    at initialization. The bucket name is read from the options of the app
    in the json file.

--- services/data_service/data_service/daos/firebase.py
+++ services/data_service/data_service/daos/firebase.py
@@ -5,9 +5,12 @@ Implements the DAO using Google Firebase.
 times, the firebase app is initialized multiple times and an exception
 is raised.
 - The authorization is read from the .env file.
+- The app config is read from the json specified in the .env file.
+- The DAO retrieves the bucket, whose name is read from the app config.
 """
 import firebase_admin
-from firebase_admin import App
+from firebase_admin import App, storage
+from google.cloud.storage.bucket import Bucket
 from data_service.daos.base import DaoBase
 
 
@@ -15,9 +18,11 @@ class FirebaseDao(DaoBase):
     """Firebase DAO"""
 
     app: App
+    bucket: Bucket
 
     def __init__(self):
         self._init_app()
+        self.bucket = storage.bucket()
 
     def _init_app(self):
         """Init a Firebase app and rises an exception if the app has already been initialized."""
@@ -27,4 +32,13 @@ class FirebaseDao(DaoBase):
             error = "Firebase app already in use or error in creating it. \
                 Check if this DAO has already been initialized and if the authorization is correct."
 
+            raise RuntimeError(error) from exc
+
+    def _get_bucket(self):
+        """Get the bucket associated to the firebase account and specified in the global setting.
+        If no bucket is found, an error is returned."""
+        try:
+            self.bucket = storage.bucket()
+        except ValueError as exc:
+            error = "Bucket not found. Check if the settings are correct."
             raise RuntimeError(error) from exc
